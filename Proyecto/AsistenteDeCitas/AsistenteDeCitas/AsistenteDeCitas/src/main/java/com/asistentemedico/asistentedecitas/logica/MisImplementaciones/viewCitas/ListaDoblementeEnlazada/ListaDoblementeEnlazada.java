b1ca2fc44440f3459aa0269b6d26e4afd85bb677
Samuel Arango Diaz <samuelarangodiaz04@gmail.com>
2024-03-29 21:27:34 -0500

    update

--- Proyecto/AsistenteDeCitas/AsistenteDeCitas/AsistenteDeCitas/src/main/java/com/asistentemedico/asistentedecitas/logica/MisImplementaciones/viewCitas/ListaDoblementeEnlazada/ListaDoblementeEnlazada.java
+++
@@ -1,9 +0,0 @@
-package com.asistentemedico.asistentedecitas.logica.MisImplementaciones.viewCitas.ListaDoblementeEnlazada;
-
-public interface ListaDoblementeEnlazada <T>{
-    public void agregarAlFinal(T valor);
-    public void agregarAlInicio(T valor);
-    public boolean estaVacia();
-    public T eliminarDelInicio();
-    public T eliminarDelFinal();
-}
